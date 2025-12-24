001ecd9c56c497f215bd3e7d6b78b617ccdeb625
rayush0325 <rayush2504@gmail.com>
2025-12-24 21:25:30 +0530

    starting project

---
+++ src/test/java/com/learning/UserAuthenticationSystem/UserAuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.UserAuthenticationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
